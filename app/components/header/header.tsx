f6c9b4dab4e1609bc38534e4d94bf258a06526f6
Gian Cabigting <gdcabigting.dev@gmail.com>
2025-01-11 00:08:18 +0800

    chore: added header

---
+++ app/components/header/header.tsx
@@ -0,0 +1,29 @@
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbList,
+  BreadcrumbPage,
+} from "@/components/ui/breadcrumb";
+import { Separator } from "@/components/ui/separator";
+import { SidebarTrigger } from "@/components/ui/sidebar";
+import { Bell } from "lucide-react";
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+      <SidebarTrigger className="-ml-1" />
+      <Separator orientation="vertical" className="mr-2 h-4" />
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbPage>Dashboard</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+    {/* <div className="flex justify-end w-[100%]">
+      <Bell className="w-6 h-6 hover:bg-gray-200 hover:rounded-full p-1"/>
+    </div> */}
+    </header>
+  );
+}
