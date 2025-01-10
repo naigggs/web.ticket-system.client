fdac3f5b608156852ed59cabc157e93f783c842d
Gian Cabigting <gdcabigting.dev@gmail.com>
2025-01-11 00:08:12 +0800

    chore: added sidebar

---
+++ app/components/sidebar/data/data.ts
@@ -0,0 +1,48 @@
+import {
+  GalleryVerticalEnd,
+  SquareTerminal,
+  TicketPlus,
+  RadioTower,
+  NotebookPen
+} from "lucide-react";
+
+export const data = {
+  user: {
+    name: "shadcn",
+    email: "m@example.com",
+    avatar: "/avatars/shadcn.jpg",
+  },
+  teams: [
+    {
+      name: "Manibaug Porac",
+      logo: GalleryVerticalEnd,
+      plan: "Ticket System",
+    },
+  ],
+  navMain: [
+    {
+      title: "Dashboard",
+      url: "/u-dashboard",
+      icon: SquareTerminal,
+      isActive: true,
+    },
+    {
+      title: "Submit Ticket",
+      url: "/u-tickets",
+      icon: TicketPlus,
+      isActive: true,
+    },
+    {
+      title: "Announcements",
+      url: "#",
+      icon: RadioTower,
+      isActive: true,
+    },
+    {
+      title: "Surveys",
+      url: "#",
+      icon: NotebookPen,
+      isActive: true,
+    },
+  ],
+};
