c852e9cc50e485b8586df190d6598bdc46cf2a04
Gian Cabigting <gdcabigting.dev@gmail.com>
2025-01-11 00:09:36 +0800

    feat: user-dashboard

---
+++ app/(user)/layout.tsx
@@ -0,0 +1,48 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono, Poppins, Inter } from "next/font/google";
+import "@/app/shared/css/globals.css";
+import {
+  SidebarInset,
+  SidebarProvider,
+} from "@/components/ui/sidebar";
+import { AppSidebar } from "@/app/components/sidebar/app-sidebar";
+import Header from "../components/header/header";
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+});
+
+const poppins = Poppins({
+  variable: "--font-poppins",
+  subsets: ["latin"],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+});
+
+export const metadata: Metadata = {
+  title: "User Dashboard",
+};
+
+export default function UserLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.variable} antialiased`}
+      >
+        <SidebarProvider>
+          <AppSidebar />
+
+          <SidebarInset>
+            <Header />
+            {children}
+          </SidebarInset>
+        </SidebarProvider>
+      </body>
+    </html>
+  );
+}
