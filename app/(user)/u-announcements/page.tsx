abca1775058219e30811e29c4bfc502c3df66608
Gian Cabigting <gdcabigting.dev@gmail.com>
2025-01-11 21:59:37 +0800

    chore: started announcement page

---
+++ app/(user)/u-announcements/page.tsx
@@ -0,0 +1,10 @@
+import AnnouncementsTable from "@/app/components/u-announcements/ticket-table";
+import React from "react";
+
+export default function AnnouncementPage() {
+  return (
+    <div className="container mx-auto py-4">
+      <AnnouncementsTable />
+    </div>
+  );
+}
