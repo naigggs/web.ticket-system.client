adf8794b90fe4f2d207e92f304ab6b73f1c5866f
Gian Cabigting <gdcabigting.dev@gmail.com>
2025-01-12 12:31:12 +0800

    ref: user tickets

--- app/(user)/u-tickets/page.tsx
+++ app/(user)/u-tickets/page.tsx
@@ -1,9 +1,9 @@
 
-import TicketTable from "@/app/components/u-tickets/ticket-table";
+import TicketsTable from "@/app/components/u-tickets/ticket-table";
 import React from "react";
 
 export default function TicketPage() {
   return <div className="container mx-auto py-4">
-    <TicketTable />
+    <TicketsTable />
   </div>;
 }
