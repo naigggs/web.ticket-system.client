8090fc306f74c5fa72b9acbcc909d20a89b283ae
Gian Cabigting <gdcabigting.dev@gmail.com>
2025-01-12 12:31:36 +0800

    feat: user survey page

---
+++ app/(user)/u-surveys/page.tsx
@@ -0,0 +1,10 @@
+import SurveysTable from "@/app/components/u-surveys/survey-table";
+import React from "react";
+
+export default function SurveyPage() {
+  return (
+    <div className="container mx-auto py-4">
+      <SurveysTable />
+    </div>
+  );
+}
