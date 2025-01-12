b7c934a73b982fde1601357e47fb0040e82faab2
jei3m <virozzegt@gmail.com>
2025-01-12 14:21:54 +0800

    feat: Admin Surveys Table

---
+++ app/(admin)/a-surveys/page.tsx
@@ -0,0 +1,12 @@
+import SurveysTable from '@/app/components/a-surveys/SurveysTable'
+import React from 'react'
+
+function page() {
+  return (
+    <div className='container mx-auto py-4'>
+        <SurveysTable/>
+    </div>
+  )
+}
+
+export default page
---
+++ app/components/a-surveys/SurveysTable.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+import React, { useState } from "react";
+import { surveys } from "@/app/data/u-dash";
+import { Calendar } from "lucide-react";
+import { motion, AnimatePresence } from "framer-motion";
+import { SurveyDatePicker } from "@/app/components/a-surveys/date-picker-surveys";
+import { Input } from "@/components/ui/input";
+
+export default function SurveysTable() {
+  const [search, setSearch] = useState("");
+  const [selectedDate, setSelectedDate] = useState<Date | null>(null);
+
+  const filteredSurveys = surveys.filter((survey) => {
+    const matchesDate =
+      !selectedDate ||
+      new Date(survey.created_at).toDateString() ===
+        selectedDate.toDateString();
+    const matchesSearch = survey.title
+      .toLowerCase()
+      .includes(search.toLowerCase());
+    return matchesDate && matchesSearch;
+  });
+
+  return (
+    <div>
+      <div className="flex flex-row justify-between items-center mb-6 mt-4">
+        <h2 className="text-2xl font-semibold ml-4">Surveys</h2>
+        <div className="flex flex-row items-center gap-2">
+          <div>
+            <Input
+              type="text"
+              placeholder="Search"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+            />
+          </div>
+          <div>
+            <SurveyDatePicker onDateChange={setSelectedDate} />
+          </div>
+          <div></div>
+        </div>
+      </div>
+      <ul className="text-gray-500">
+        <AnimatePresence>
+          {filteredSurveys.map((survey, index) => (
+            <motion.li
+              key={survey.id}
+              className={`border-b py-5 px-5 hover:bg-gray-50 ${
+                index === 0 ? "border-t" : ""
+              }`}
+              initial={{ opacity: 0, y: 10 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -10 }}
+              transition={{ duration: 0.3 }}
+            >
+              <div className="uppercase font-bold text-black hover:underline">
+                {survey.title}
+              </div>
+
+              <div className="text-sm my-2 line-clamp-3">
+                {survey.description}
+              </div>
+              <div>
+                <span className="text-gray-400 text-sm flex flex-row items-center">
+                  <Calendar className="h-3.5 -mt-0.5 w-auto mr-1" />
+                  {survey.created_at}
+                </span>
+              </div>
+            </motion.li>
+          ))}
+        </AnimatePresence>
+      </ul>
+    </div>
+  );
+}
