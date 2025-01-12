492d6c1defd6ff4c49a1a22f6b665fb7b2b9d7c4
jei3m <virozzegt@gmail.com>
2025-01-12 14:22:08 +0800

    feat: a-surveys date-picker component

---
+++ app/components/a-surveys/date-picker-surveys.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { format } from "date-fns";
+import { CalendarIcon } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { SurveyDatePickerProps } from "./types";
+
+export function SurveyDatePicker({
+  onDateChange,
+}: Readonly<SurveyDatePickerProps>) {
+  const [date, setDate] = useState<Date | null>(null);
+  console.log(date);
+
+  useEffect(() => {
+    onDateChange(date);
+  }, [date, onDateChange]);
+
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button
+          variant={"outline"}
+          className={cn(
+            "w-full justify-start text-left font-normal",
+            !date && "text-muted-foreground"
+          )}
+        >
+          <CalendarIcon />
+          {date ? format(date, "PPP") : <span>Pick a date</span>}
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-auto p-0" align="start">
+        <Calendar
+          mode="single"
+          selected={date ?? undefined}
+          onSelect={(day) => setDate(day ?? null)}
+          initialFocus
+        />
+      </PopoverContent>
+    </Popover>
+  );
+}
