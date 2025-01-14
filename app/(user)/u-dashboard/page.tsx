1868e2923d692b3d0eba0e9861ac21407e9e834c
Gian Cabigting <gdcabigting.dev@gmail.com>
2025-01-14 18:54:47 +0800

    style: reduced padding

--- app/(user)/u-dashboard/page.tsx
+++ app/(user)/u-dashboard/page.tsx
@@ -5,11 +5,10 @@ import SurveyCard from "@/app/components/u-dashboard/survey-card";
 
 export default function UserDashboard() {
   return (
-    <div className="container mx-auto px-4 my-5 md:my-20">
+    <div className="container mx-auto px-4 my-5">
       <div className="text-4xl font-bold text-center md:text-left">
         Goodmorning User!
       </div>
-
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5 mt-4">
         <TicketCard />
         <AnnouncementCard />
