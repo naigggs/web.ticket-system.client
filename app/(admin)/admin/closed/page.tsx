980b27510484f03bf1757ff895e43422341dafd7
jei3m <virozzegt@gmail.com>
2025-01-15 22:53:10 +0800

    feat: route for closed tickets page

---
+++ app/(admin)/admin/closed/page.tsx
@@ -0,0 +1,13 @@
+import AnnouncementsTable from '@/app/components/admin-components/a-announcements/announcement-table'
+import ClosedPage from '@/app/components/admin-components/a-closed/closed-page'
+import React from 'react'
+
+function Closed() {
+  return (
+    <div>
+        <ClosedPage/>
+    </div>
+  )
+}
+
+export default Closed
