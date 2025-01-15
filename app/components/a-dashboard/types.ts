d4f42e007d229f6568d0dba0a2a71c115323989e
jei3m <virozzegt@gmail.com>
2025-01-15 21:23:50 +0800

    ref: changed directory of all admin page components

--- app/components/a-dashboard/types.ts
+++
@@ -1,8 +0,0 @@
-export type Tickets = {
-    id: number;
-    title: string;
-    description: string;
-    status: "Open" | "In Progress" | "Closed" | "On Hold";
-    location: string;
-    created_at: string;
-  };
