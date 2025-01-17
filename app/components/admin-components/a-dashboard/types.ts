b7617cf3aae7946eb91ac95a1caa8afe200fcc49
jei3m <virozzegt@gmail.com>
2025-01-17 08:00:24 +0800

    ref: update Tickets type with additional fields and fix formatting

--- app/components/admin-components/a-dashboard/types.ts
+++ app/components/admin-components/a-dashboard/types.ts
@@ -1,8 +1,36 @@
+// export type Tickets = {
+//     id: number;
+//     title: string;
+//     description: string;
+//     status: "Open" | "In Progress" | "Closed" | "On Hold";
+//     location: string;
+//     created_at: string;
+//   };
+
 export type Tickets = {
-    id: number;
-    title: string;
-    description: string;
-    status: "Open" | "In Progress" | "Closed" | "On Hold";
-    location: string;
-    created_at: string;
-  };
+  id: number;
+  title: string;
+  description: string;
+  status: string
+  location: string;
+  created_at: string;
+  ticket_status: "Open" | "In Progress" | "Closed" | "On Hold";
+  address: string;
+  concern_type: string;
+  attachment_1: string;
+  attachment_2: string;
+  name: string;
+  birthdate: string;
+  age: number;
+  birthplace: string;
+  height: string;
+  weight: string;
+  civil_status: string;
+  contact_person_name: string;
+  contact_person_number: string;
+  purpose: string;
+};
+
+export interface TicketDatePickerProps {
+  onDateChange: (date: Date | null) => void;
+}
