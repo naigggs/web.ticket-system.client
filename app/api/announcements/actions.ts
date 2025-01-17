ea5b3b27d369f477f181a3f6ce89ed875e998b56
jei3m <virozzegt@gmail.com>
2025-01-17 20:04:21 +0800

    feat: Create announcement endpoint

---
+++ app/api/announcements/actions.ts
@@ -0,0 +1,34 @@
+import { createClient } from "@/utils/supabase/server";
+
+export async function createAnnouncement(body: {
+    title: string;
+    description: string;
+    subtitle: string;
+    body: string;
+  }) {
+    const supabase = await createClient();
+    try {
+      const { title, description, subtitle, body: announcementBody } = body;
+  
+      const { data, error } = await supabase
+        .from("announcements")
+        .insert([
+          {
+            title,
+            description,
+            subtitle,
+            body: announcementBody,
+            created_at: new Date().toISOString(),
+          },
+        ])
+        .select();
+  
+      if (error) {
+        throw new Error(error.message);
+      }
+  
+      return data;
+    } catch (error: any) {
+      throw new Error(error.message);
+    }
+  }
