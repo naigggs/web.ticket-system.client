a44d1b9f65c0410cdd89c4e4eb0327f27256aaea
Gian Cabigting <gdcabigting.dev@gmail.com>
2025-01-22 10:42:13 +0800

    feat: added charts

--- app/(user)/user/dashboard/page.tsx
+++ app/(user)/user/dashboard/page.tsx
@@ -2,12 +2,18 @@ import React from "react";
 import TicketCard from "@/app/components/user-components/u-dashboard/ticket-card";
 import AnnouncementCard from "@/app/components/user-components/u-dashboard/announcement-card";
 import SurveyCard from "@/app/components/user-components/u-dashboard/survey-card";
+import { TicketSubmitted } from "@/app/components/user-components/u-dashboard/ticket-submitted";
+import { TicketStatus } from "@/app/components/user-components/u-dashboard/ticket-status";
 
 export default function UserDashboard() {
   return (
     <div className="container mx-auto py-4 px-4 sm:px-6 lg:px-8">
       <div className="text-4xl font-bold text-center md:text-left">
         Goodmorning User!
+      </div>
+      <div className="flex flex-row justify-center space-x-4 items-center mt-4">
+        <TicketSubmitted />
+        <TicketStatus /> 
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5 mt-4">
         <div className="col-span-1 sm:col-span-2 lg:col-span-4">
