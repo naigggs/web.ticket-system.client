914355ccbc304e57626137b2d16a091c11d6467c
Gian Cabigting <gdcabigting.dev@gmail.com>
2025-01-22 10:41:34 +0800

    feat: added sidebar for staff

---
+++ app/components/staff-components/a-sidebar/app-sidebar.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import * as React from "react";
+import { NavMain } from "@/app/components/sidebar/nav-main";
+import { NavUser } from "@/app/components/sidebar/nav-user";
+import { TeamSwitcher } from "@/app/components/sidebar/team-switcher";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarRail,
+} from "@/components/ui/sidebar";
+import { data } from "./data/data";
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="icon" {...props}>
+      <SidebarHeader>
+        <TeamSwitcher teams={data.teams} />
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+      <SidebarRail />
+    </Sidebar>
+  );
+}
---
+++ app/components/staff-components/a-sidebar/data/data.ts
@@ -0,0 +1,57 @@
+import {
+  GalleryVerticalEnd,
+  SquareTerminal,
+  TicketPlus,
+  RadioTower,
+  NotebookPen,
+  UserRoundCog,
+  Ticket,
+  TicketXIcon
+} from "lucide-react";
+
+export const data = {
+  user: {
+    name: "shadcn",
+    email: "m@example.com",
+    avatar: "/avatars/shadcn.jpg",
+  },
+  teams: [
+    {
+      name: "Manibaug Porac",
+      logo: GalleryVerticalEnd,
+      plan: "Ticket System",
+    },
+  ],
+  navMain: [
+    {
+      title: "Task Board",
+      url: "/admin/dashboard",
+      icon: SquareTerminal,
+      isActive: true,
+    },
+    {
+      title: "Closed Tickets",
+      url: "/admin/closed",
+      icon: TicketXIcon,
+      isActive: true,
+    },
+    {
+      title: "Manage Accounts",
+      url: "/admin/accounts",
+      icon: UserRoundCog,
+      isActive: true,
+    },
+    {
+      title: "Announcements",
+      url: "/admin/announcements",
+      icon: RadioTower,
+      isActive: true,
+    },
+    {
+      title: "Surveys",
+      url: "/admin/surveys",
+      icon: NotebookPen,
+      isActive: true,
+    },
+  ],
+};
