092cc40380ccbcf3bfd1d79f7320549388f772f6
Gian Cabigting <gdcabigting.dev@gmail.com>
2025-01-22 10:42:25 +0800

    feat: added staff pages

---
+++ app/(staff)/staff/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import DashboardPage from '@/app/components/admin-components/a-dashboard/dashboard-page'
+import React from 'react'
+
+export default function StaffDashboard() {
+  return (
+    <div>
+      <DashboardPage/>
+    </div>
+  )
+}
