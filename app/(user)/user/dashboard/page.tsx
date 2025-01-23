6ed32c95a59abfa7742f3a2d75f29da0606eb9f6
Gian Cabigting <gdcabigting.dev@gmail.com>
2025-01-23 11:48:27 +0800

    style: copied the design of the admin dashboard

--- app/(user)/user/dashboard/page.tsx
+++ app/(user)/user/dashboard/page.tsx
@@ -1,28 +1,68 @@
-import React from "react";
+'use client';
+
+import React, { useEffect, useState } from "react";
 import TicketCard from "@/app/components/user-components/u-dashboard/ticket-card";
 import AnnouncementCard from "@/app/components/user-components/u-dashboard/announcement-card";
 import SurveyCard from "@/app/components/user-components/u-dashboard/survey-card";
 import { TicketSubmitted } from "@/app/components/user-components/u-dashboard/ticket-submitted";
 import { TicketStatus } from "@/app/components/user-components/u-dashboard/ticket-status";
+import { Ticket } from "lucide-react";
+import { User } from "@/app/components/sidebar/types";
 
 export default function UserDashboard() {
+  const [user, setUser] = useState<User>();
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    async function fetchUserInfo() {
+      try {
+        const response = await fetch(`/api/user`);
+        if (!response.ok) {
+          throw new Error("Failed to fetch user data");
+        }
+        const data = await response.json();
+        if (data && data.length > 0) {
+          setUser(data[0]);
+        }
+      } catch (err) {
+        setError(
+          err instanceof Error ? err.message : "An unknown error occurred"
+        );
+      }
+    }
+
+    fetchUserInfo();
+  }, []);
+
   return (
-    <div className="container mx-auto py-4 px-4 sm:px-6 lg:px-8">
-      <div className="text-4xl font-bold text-center md:text-left">
-        Goodmorning User!
+    <div className="px-10 my-6">
+      <div className="flex justify-between mb-4">
+        <h2 className="text-xl md:text-2xl flex items-center font-bold text-center">
+          Goodmorning {user?.first_name} {user?.last_name}!
+        </h2>
       </div>
-      <div className="flex flex-row justify-center space-x-4 items-center mt-4">
-        <TicketSubmitted />
-        <TicketStatus /> 
-      </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5 mt-4">
-        <div className="col-span-1 sm:col-span-2 lg:col-span-4">
+      <div className="grid grid-cols-3 gap-6">
+        {/* Left Side: Tickets */}
+        <div className="flex flex-col gap-3 animate-in fade-in slide-in-from-bottom-8 duration-300">
+          <div className="px-4 py-2 bg-green-300 rounded-2xl font-bold">
+            Tickets
+          </div>
           <TicketCard />
         </div>
-        <div className="col-span-1 sm:col-span-1 lg:col-span-2">
+
+        {/* Center: Announcements */}
+        <div className="flex flex-col gap-3 animate-in fade-in slide-in-from-bottom-8 duration-500">
+          <div className="px-4 py-2 bg-blue-300 rounded-2xl font-bold">
+            Announcements
+          </div>
           <AnnouncementCard />
         </div>
-        <div className="col-span-1 sm:col-span-1 lg:col-span-2">
+
+        {/* Right Side: Surveys */}
+        <div className="flex flex-col gap-3 animate-in fade-in slide-in-from-bottom-8 duration-700">
+          <div className="px-4 py-2 bg-red-400 rounded-2xl font-bold">
+            Surveys
+          </div>
           <SurveyCard />
         </div>
       </div>
