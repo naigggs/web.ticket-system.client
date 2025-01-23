71a63f50c6eb5d22c5bd8f11ae08047cdda728ef
Gian Cabigting <gdcabigting.dev@gmail.com>
2025-01-23 11:48:08 +0800

    feat: fetch user name in sidebar

--- app/components/sidebar/nav-user.tsx
+++ app/components/sidebar/nav-user.tsx
@@ -26,18 +26,37 @@ import {
   useSidebar,
 } from "@/components/ui/sidebar";
 import { createClient } from "@/utils/supabase/client";
+import { useEffect, useState } from "react";
+import { User } from "./types";
 
-export function NavUser({
-  user,
-}: {
-  user: {
-    name: string;
-    email: string;
-    avatar: string;
-  };
-}) {
+export function NavUser() {
   const { isMobile } = useSidebar();
   const supabase = createClient();
+
+  const [user, setUser] = useState<User>();
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    async function fetchUserInfo() {
+      try {
+        const response = await fetch(`/api/user`);
+        if (!response.ok) {
+          throw new Error("Failed to fetch user data");
+        }
+        const data = await response.json();
+        if (data && data.length > 0) {
+          setUser(data[0]); // Set the first user object from the array
+        }
+      } catch (err) {
+        setError(
+          err instanceof Error ? err.message : "An unknown error occurred"
+        );
+      }
+    }
+
+    fetchUserInfo();
+  }, []);
+
   return (
     <SidebarMenu>
       <SidebarMenuItem>
@@ -48,12 +67,22 @@ export function NavUser({
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
               <Avatar className="h-8 w-8 rounded-lg">
-                <AvatarImage src={user.avatar} alt={user.name} />
-                <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+                {/* <AvatarImage src={user?.avatar} alt={user?.name} /> */}
+                <AvatarFallback className="rounded-lg">
+                 {user?.first_name[0]}{user?.last_name[0]}
+                </AvatarFallback>
               </Avatar>
               <div className="grid flex-1 text-left text-sm leading-tight">
-                <span className="truncate font-semibold">{user.name}</span>
-                <span className="truncate text-xs">{user.email}</span>
+                {user ? (
+                  <>
+                    <span className="truncate font-semibold">
+                      {user.first_name} {user.last_name}
+                    </span>
+                    <span className="truncate text-xs">{user.location}</span>
+                  </>
+                ) : (
+                  <span className="truncate font-light">Loading...</span>
+                )}
               </div>
               <ChevronsUpDown className="ml-auto size-4" />
             </SidebarMenuButton>
@@ -67,12 +96,22 @@ export function NavUser({
             <DropdownMenuLabel className="p-0 font-normal">
               <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                 <Avatar className="h-8 w-8 rounded-lg">
-                  <AvatarImage src={user.avatar} alt={user.name} />
-                  <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+                  {/* <AvatarImage src={user?.avatar} alt={user?.name} /> */}
+                  <AvatarFallback className="rounded-lg">
+                  {user?.first_name[0]}{user?.last_name[0]}
+                  </AvatarFallback>
                 </Avatar>
                 <div className="grid flex-1 text-left text-sm leading-tight">
-                  <span className="truncate font-semibold">{user.name}</span>
-                  <span className="truncate text-xs">{user.email}</span>
+                  {user ? (
+                    <>
+                      <span className="truncate font-semibold">
+                        {user.first_name} {user.last_name}
+                      </span>
+                      <span className="truncate text-xs">{user.location}</span>
+                    </>
+                  ) : (
+                    <span className="truncate font-light">Loading...</span>
+                  )}
                 </div>
               </div>
             </DropdownMenuLabel>
