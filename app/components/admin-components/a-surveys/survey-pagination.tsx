bb4ca70965551eb46445129f17464f1b2a701868
jei3m <virozzegt@gmail.com>
2025-01-23 20:26:34 +0800

    ref: pagination into component

---
+++ app/components/admin-components/a-surveys/survey-pagination.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import React from "react";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationItem,
+  PaginationLink,
+  PaginationPrevious,
+  PaginationNext,
+} from "@/components/ui/pagination";
+
+interface SurveyPagination {
+  currentPage: number;
+  totalPages: number;
+  onPageChange: (page: number) => void;
+}
+
+export const SurveyPagination: React.FC<SurveyPagination> = ({
+  currentPage,
+  totalPages,
+  onPageChange,
+}) => {
+  return (
+    <Pagination>
+      <PaginationContent>
+        <PaginationItem>
+          <PaginationPrevious
+            href="#"
+            onClick={(e) => {
+              if (currentPage === 1) e.preventDefault();
+              else onPageChange(currentPage - 1);
+            }}
+            className={currentPage === 1 ? "pointer-events-none opacity-50" : ""}
+          />
+        </PaginationItem>
+        {Array.from({ length: totalPages }, (_, i) => i + 1).map((page) => (
+          <PaginationItem key={page}>
+            <PaginationLink
+              href="#"
+              isActive={page === currentPage}
+              onClick={() => onPageChange(page)}
+            >
+              {page}
+            </PaginationLink>
+          </PaginationItem>
+        ))}
+        <PaginationItem>
+          <PaginationNext
+            href="#"
+            onClick={(e) => {
+              if (currentPage === totalPages) e.preventDefault();
+              else onPageChange(currentPage + 1);
+            }}
+            className={
+              currentPage === totalPages ? "pointer-events-none opacity-50" : ""
+            }
+          />
+        </PaginationItem>
+      </PaginationContent>
+    </Pagination>
+  );
+};
